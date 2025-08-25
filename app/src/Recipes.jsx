71c5966a08497c35313e8cfe91dd7209ead64036
DraysenOrosco <Oroscodraysen@gmail.com>
2025-08-25 14:34:39 -0700

    Day1

--- app/src/Recipes.jsx
+++ app/src/Recipes.jsx
@@ -15,7 +15,9 @@ export default function Recipes () {
       
       <div className='right-Section'>
       <h3 className="instructionsTitle">instructions</h3>
-      <p className="instructionsDesc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, molestiae consequuntur. Nobis vero voluptatem modi perspiciatis deleniti vel, veniam architecto repudiandae sint nostrum nisi molestiae officia numquam quidem earum pariatur!</p>
+      <input type='checkbox' className='recipeCheckbox' name='recipeCheckbox' value='recipe'/>
+      <label for='checkbox1'>lorem</label>
+      {/* <p className="instructionsDesc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, molestiae consequuntur. Nobis vero voluptatem modi perspiciatis deleniti vel, veniam architecto repudiandae sint nostrum nisi molestiae officia numquam quidem earum pariatur!</p> */}
 
       <img className='ChefPic' 
       src='https://i.pinimg.com/564x/ae/31/62/ae316256daf28f5ed4a8be128d4c4ec7.jpg' 
