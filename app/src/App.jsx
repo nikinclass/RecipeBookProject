59f1c2e4dbe0670b6e4b3601d10206cf2f09e86e
DraysenOrosco <Oroscodraysen@gmail.com>
2025-08-25 08:32:31 -0700

    routers

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
-import Home from './Home.jsx'
-import '../styles/App.css'
+import {Routes, Route } from "react-router";
+import { useeffect, useState } from 'react';
+import Home from './Home.jsx';
+import Recipes from './Recipes.jsx';
+import Profile from './Profile.jsx';
+import Search from './Search.jsx';
+import Search from './Search.jsx';
+import Favorites from './Favorites.jsx'
+import Blog from './Blog.jsx'
+import '../styles/App.css';
+
+import "../styles/App.css";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/" element={<Recipes />} /> 
+        <Route path="/" element={<Profile />} />
+        <Route path="/" element={< Search/>} />
+        <Route path="/" element={<Favorites />} />
+        <Route path="/" element={<Blog />} />
+      </Routes>
     </>
   )
 }
