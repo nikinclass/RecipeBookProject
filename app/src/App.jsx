1c53482b62bb0c5541e097bc2b36499bde15efcb
Nikki <ngabor@mail.weber.edu>
2025-08-25 09:44:16 -0600

    import merge

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,12 @@
 import {Routes, Route } from "react-router";
 import { useeffect, useState } from 'react';
 import Home from './Home.jsx';
-import Recipes from './Recipes.jsx';
-import Profile from './Profile.jsx';
-import Search from './Search.jsx';
-import Search from './Search.jsx';
-import Favorites from './Favorites.jsx'
-import Blog from './Blog.jsx'
+// import Recipes from './Recipes.jsx';
+// import Profile from './Profile.jsx';
+// import Search from './Search.jsx';
+// import Search from './Search.jsx';
+// import Favorites from './Favorites.jsx'
+// import Blog from './Blog.jsx'
 import '../styles/App.css';
 
 import "../styles/App.css";
