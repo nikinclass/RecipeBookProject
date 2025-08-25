327dde553737e62958fef5b8af41bfea4a4ce82c
Alec <alecv1528@gmail.com>
2025-08-25 09:35:05 -0700

    app.js refactored to working state

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,43 +5,8 @@
  */
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
+    development: {
+        client: 'pg',
+        connection: 'postgresql://postgres:docker@localhost:5432/backend-db'
     }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+}
---
+++ server/migrations/20250825160559_create_users_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('users', (table) => {
+        table.increments();
+        table.string('name', 20).notNullable();
+        table.string('email', 50).notNullable();
+        table.string('role', 30).notNullable();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
