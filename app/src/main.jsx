797cb6cc48af11c9ec52de0ab4ba747905f20cfb
DraysenOrosco <Oroscodraysen@gmail.com>
2025-08-25 09:16:30 -0700

    updated routes

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-dom-router'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '../styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
