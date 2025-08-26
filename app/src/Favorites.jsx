c6ed8d95de7900945656a7e39856e6eed29f4bf8
DraysenOrosco <Oroscodraysen@gmail.com>
2025-08-26 14:47:53 -0700

    favorite page update

--- app/src/Favorites.jsx
+++ app/src/Favorites.jsx
@@ -1,5 +1,35 @@
+import '../styles/Favorites.css';
+
 export default function Favorites() {
-    return (
-        <h1>Favorites</h1>
-    )
-    }
+  return (
+    <div className="favoritesSection">
+      <div className="photoCard">
+        {/* First Image */}
+        <div className="photoItem">
+          <h2 className="photoTitle">Photo 1</h2>
+          <img
+            src="https://www.kroger.com/product/images/xlarge/front/0007066201003"
+            alt="Photo 1"
+          />
+          <p className="photoDescription">This cool</p>
+        </div>
+
+        {/* Second Image */}
+        <div className="photoItem">
+          <h2 className="photoTitle">Photo 2</h2>
+          <img
+            src="https://www.kroger.com/product/images/xlarge/front/0007066201003"
+            alt="Photo 2"
+          />
+          <p className="photoDescription">Favorite Image</p>
+        </div>
+
+        <div className='photoItem'>
+            <h2 className='photoTitle'>Photo 3</h2>
+            <img src='https://www.kroger.com/product/images/xlarge/front/0007066201003' alt='Photo 3' />
+            <p className="photoDescription">Favorite Image</p>
+        </div>
+      </div>
+    </div>
+  );
+}
