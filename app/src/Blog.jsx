0d1048e163cfa537ed6637fb9e5bb4f3125da07c
DraysenOrosco <Oroscodraysen@gmail.com>
2025-08-26 09:05:10 -0700

    updated page

--- app/src/Blog.jsx
+++ app/src/Blog.jsx
@@ -1,5 +1,14 @@
 export default function Blog() {
 return (
-    <h1> Blog</h1>
+    <div>
+    <div className="BlogTitleContainer">
+        <h1 className="BlogTitle"> Blog</h1>
+    </div>
+    
+    <div className="openingContainer">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident vel magni blanditiis sunt! In atque consequatur eveniet reprehenderit commodi amet adipisci tempore neque beatae ut accusantium consectetur eaque, laborum sed.</p>
+        
+    </div>
+</div>
 )
 }
