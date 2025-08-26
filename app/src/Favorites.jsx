da33333913306a952ede84dcb38af31cd2b48624
DraysenOrosco <Oroscodraysen@gmail.com>
2025-08-26 14:16:37 -0700

    small update

--- app/src/Favorites.jsx
+++ app/src/Favorites.jsx
@@ -1,5 +1,15 @@
 export default function Favorites() {
     return (
-        <h1>Favorites</h1>
+        <>
+        <div className="FavoriteContainer">
+        <h1 className="favoriteTitle">Favorites</h1>
+        <img src="https://cdn-icons-png.freepik.com/512/6852/6852344.png" alt="heartImg" />
+        </div>
+        
+        <div className="favRecipies">
+            <img className="favImg" src="https://assets-jpcust.jwpsrv.com/thumbnails/mCITeuJc-720.jpg" alt="Favriote Image" />
+            
+        </div>
+        </>
     )
     }
