7ff50e24031a2bf19ed8161d12ec29a31a43b4eb
yepitsaname <fernando.g.berry@gmail.com>
2025-08-26 14:38:11 -0400

    adds tests for login functionality and verifies display componenets of the account creation form

--- app/components/Account.jsx
+++ app/components/Account.jsx
@@ -1,11 +1,23 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Account(){
+  const [validCredentials, setValidCredentials ] = useState(true);
+  const [createAccount, setCreateAccount] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
+    fetch('http://localhost:8080/')
+      .then(res => {
+        if(res.status == 401){
+          setValidCredentials(false);
+        } else if(res.status == 200){
+          setValidCredentials(true);
+          navigate('/');
+        }
+      })
+      .catch(err => console.error(err));
     console.log(e.target[0].value, e.target[1].value);
   }
 
@@ -13,15 +25,36 @@ export default function Account(){
     navigate('/');
   }
 
+  if( createAccount ){
+    return (
+      <form onSubmit={(e)=>{ handleSubmit(e) }}>
+        <h2>Account Creation</h2>
+        <label htmlFor="email">Email Address</label>
+        <input type="email" name="" id="verify_email" placeholder="example@example.com" />
+        <label htmlFor="verify_email">Confirm Email</label>
+        <input type="email" name="" id="email" placeholder="example@example.com" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="" id="password" placeholder="password"/>
+        <label htmlFor="verify_password">Confirm Password</label>
+        <input type="password" name="" id="verify_password" placeholder="confirm"/>
+        <button type="submit" value="login">Create Account</button>
+        <button type="button" onClick={()=>{ handleCancel() }}>Cancel</button>
+        <p>Returning user? <a onClick={()=>{setCreateAccount(false)}}>login</a></p>
+      </form>
+    )
+  }
+
   return (
     <form onSubmit={(e)=>{ handleSubmit(e) }}>
-      <label htmlFor="email">Email Address</label>
+      <label htmlFor="email">Email Address {
+        validCredentials ? "" : <span>Invalid email/password</span>
+      }</label>
       <input type="email" name="" id="email" placeholder="example@example.com" />
       <label htmlFor="password">Password</label>
       <input type="password" name="" id="password" placeholder="********"/>
       <button type="submit" value="login">Login</button>
       <button type="button" onClick={()=>{ handleCancel() }}>Cancel</button>
-      <p>New user? <a>create account</a></p>
+      <p>New user? <a onClick={()=>{setCreateAccount(true)}}>create account</a></p>
     </form>
   )
 }
--- app/test/Account.test.jsx
+++ app/test/Account.test.jsx
@@ -1,13 +1,16 @@
 import { getByText, render, screen } from '@testing-library/react';
 import Account from '../components/Account';
 import App from '../src/App';
-import { describe, expect, vi} from 'vitest';
+import { describe, expect, it, vi} from 'vitest';
 import { userEvent } from '@testing-library/user-event';
 
 global.fetch = vi.fn()
 
-function fetchResponse(data) {
-  return { json: () => new Promise(resolve => resolve(data)) };
+function fetchResponse(data, statusCode) {
+  return {
+    status: statusCode,
+    json: () => new Promise(resolve => resolve(data))
+  };
 };
 
 const mockNavigate = vi.fn();
@@ -31,6 +34,7 @@ describe("Test login presentation", () => {
     let emailInput = screen.getByPlaceholderText('example@example.com');
     expect(emailInput).toBeInTheDocument();
   });
+
   it("Should request the user password", () => {
     let password = screen.getByText('Password');
     expect(password).toBeInTheDocument();
@@ -38,6 +42,7 @@ describe("Test login presentation", () => {
     let passwordInput = screen.getByPlaceholderText('********');
     expect(passwordInput).toBeInTheDocument();
   });
+
   it("Should have a login and cancel button", () => {
     let loginButton = screen.getByText('Login');
     expect(loginButton).toBeInTheDocument();
@@ -45,6 +50,7 @@ describe("Test login presentation", () => {
     let cancelButton = screen.getByText('Cancel');
     expect(cancelButton).toBeInTheDocument();
   });
+
   it("Should have a create Account link", () => {
     let createAccount = screen.getByText('create account');
     expect(createAccount).toBeInTheDocument();
@@ -52,21 +58,104 @@ describe("Test login presentation", () => {
 });
 
 describe("Test login functions", ()=>{
-  beforeEach(()=>{
+  beforeEach(async ()=>{
     render(<App/>);
+
   });
 
   it("Should notify the user of a failed log in attempt", async () => {
-    let login = screen.getByText('Login/Create Account')
-    await userEvent.click(login)
+    fetch.mockResolvedValue(fetchResponse(null,401));
+
+    let login = screen.getByText('Login/Create Account');
+    await userEvent.click(login);
 
     let loginButton = screen.getByText('Login');
     await userEvent.click(loginButton);
 
-    //let errorText = screen.getByText('Invalid email/password');
-    //expect(errorText).toBeInTheDocument();
+    let errorText = screen.getByText('Invalid email/password');
+    expect(errorText).toBeInTheDocument();
   });
-  it("Should navigate to a logged in view on successful login", () => {});
-  it("Should navigate to a logged out view on cancel", () => {});
-  it("Should return a cookie that authorizes the user", () => {});
+
+  it("Should navigate to a logged in home view on successful login", async () => {
+    fetch.mockResolvedValue(fetchResponse(null,200));
+    let login = screen.getByText('Login/Create Account');
+    await userEvent.click(login);
+
+    let loginButton = screen.getByText('Login');
+    await userEvent.click(loginButton);
+
+    let profile = screen.getByText('Profile');
+    expect(profile).toBeInTheDocument();
+  });
+
+  // it("Should navigate to a not logged in home view on cancel", async () => {
+  //   let login = screen.getByText('Login/Create Account');
+  //   await userEvent.click(login);
+
+  //   let loginButton = screen.getByText('Login');
+  //   await userEvent.click(loginButton);
+
+  //   let profile = screen.getByText('Profile');
+  //   expect(profile).not.toBeInTheDocument();
+  // });
+
+  it("Should pass user credentials to the API and receive a auth token", async () => {
+
+  });
+})
+
+describe("Test create user presentation", () => {
+  beforeEach( async ()=>{
+    render(<Account/>);
+  });
+
+  it("Should request the user email", async () => {
+    let createAccount = screen.getByText('create account');
+    await userEvent.click(createAccount);
+
+    let email = screen.getByText('Email Address');
+    expect(email).toBeInTheDocument();
+
+    let confirmEmail = screen.getByText('Confirm Email');
+    expect(confirmEmail).toBeInTheDocument();
+
+    let emailInputs = screen.getAllByPlaceholderText('example@example.com');
+    expect(emailInputs.length).toBe(2);
+  });
+
+  it("Should request the user password", async () => {
+    let createAccount = screen.getByText('create account');
+    await userEvent.click(createAccount);
+
+    let password = screen.getByText('Password');
+    expect(password).toBeInTheDocument();
+
+    let confirmPassword = screen.getByText('Confirm Password');
+    expect(confirmPassword).toBeInTheDocument();
+
+    let passwordInput = screen.getByPlaceholderText('password');
+    expect(passwordInput).toBeInTheDocument();
+
+    let confirmPasswordInput = screen.getByPlaceholderText('confirm');
+    expect(confirmPasswordInput).toBeInTheDocument();
+  });
+
+  it("Should have a create account and cancel button", async () => {
+    let createAccount = screen.getByText('create account');
+    await userEvent.click(createAccount);
+
+    let createButton = screen.getByText('Create Account');
+    expect(createButton).toBeInTheDocument();
+
+    let cancelButton = screen.getByText('Cancel');
+    expect(cancelButton).toBeInTheDocument();
+  });
+
+  it("Should have a create Account link", async () => {
+    let createAccount = screen.getByText('create account');
+    await userEvent.click(createAccount);
+
+    let login = screen.getByText('login');
+    expect(login).toBeInTheDocument();
+  });
 })
