d4d736c714384a79403fd2c773d47fd4e1edf211
Nikki <ngabor@mail.weber.edu>
2025-08-26 13:01:18 -0600

    changes to sidebar jsx, app, and sidebar styling

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -1,11 +1,15 @@
+import '/styles/Sidebar.css'
+
 export default function Sidebar ({setter}) {
   return (
-    <nav className='sidebar'>
-      <a onClick={() => {setter('breakfast')}}>Breakfast</a>
-      <a onClick={() => {setter('lunch')}}>Lunch</a>
-      <a onClick={() => {setter('dinner')}}>Dinner</a>
-      <a onClick={() => {setter('dessert')}}>Dessert</a>
-      <a onClick={() => {setter('drinks')}}>Drinks</a>
-    </nav>
+    <div className='flex-container'>
+      <nav className='sidebar' id='sidebar-content' >
+        <a onClick={() => {setter('breakfast')}}>Breakfast</a>
+        <a onClick={() => {setter('lunch')}}>Lunch</a>
+        <a onClick={() => {setter('dinner')}}>Dinner</a>
+        <a onClick={() => {setter('dessert')}}>Dessert</a>
+        <a onClick={() => {setter('drinks')}}>Drinks</a>
+      </nav>
+    </div>
   )
 }
