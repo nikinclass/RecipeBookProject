26db5e86ad38069e4776d210aece6083141e05cd
homelander <michaelalexburton@protonmail.com>
2025-08-27 10:35:19 -0400

    login endpoint still deleted

--- server/app.js
+++ server/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const app = express();
-const knex = require("knex")(require("./knexfile.js")["docker"]);//If testing endpoints, environment must be the 'test' environment from the knex file
+const knex = require("knex")(require("./knexfile.js")["docker"]); //If testing endpoints, environment must be the 'test' environment from the knex file
 const port = 8080;
-const cors = require('cors')
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
-
 
 app.get("/", (req, res) => {
   res.send(`Express listening on port: ${port}`);
@@ -14,51 +13,46 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-    knex("users")
-        .select("*")
-        .then((data) => {
-            return res.status(200).json(data);
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json({ error: "failed to fetch users" });
-        });
-
+  knex("users")
+    .select("*")
+    .then((data) => {
+      return res.status(200).json(data);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: "failed to fetch users" });
+    });
 });
 
 app.get("/recipes", (req, res) => {
-    knex("recipes")
-        .select("*")
-        .then((data) => res.status(200).json(data));
+  knex("recipes")
+    .select("*")
+    .then((data) => res.status(200).json(data));
 });
 
-
-
 app.post("/users", (req, res) => {
-    knex('users')
-        .select("*")
-        .then( userArray => {
-            for (let user of userArray) {
-                if (req.body.email.toLowerCase() === user.email.toLowerCase()){
-                    res.status(401).send("User already exists")
-                    return;
-                }
-            }
-            knex('users')
-                .insert({
-                    name: "default",
-                    email: req.body.email,
-                    role: 'admin',
-                    password: req.body.password
-                })
-                .catch((err) => {
-                    if (err) console.log(err);
-                })
-            res.status(200)
+  knex("users")
+    .select("*")
+    .then((userArray) => {
+      for (let user of userArray) {
+        if (req.body.email.toLowerCase() === user.email.toLowerCase()) {
+          res.status(401).send("User already exists");
+          return;
+        }
+      }
+      knex("users")
+        .insert({
+          name: "default",
+          email: req.body.email,
+          role: "admin",
+          password: req.body.password,
         })
-})
-
-
+        .catch((err) => {
+          if (err) console.log(err);
+        });
+      res.status(200);
+    });
+});
 
 module.exports = app.listen(port, () => {
   console.log(`Express listening on port: ${port}`);
